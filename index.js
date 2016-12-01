2fee5a9242226d2762a00dd952d614b50802a1b1
aleksandar <93lukic@live.com>
2016-12-01 02:47:59 +0100

    Project upload

---
+++ index.js
@@ -0,0 +1,4 @@
+
+module.exports.ClusterManager = require('./lib/manager/ClusterManager');
+module.exports.DefaultClusterManager = require('./lib/manager/default/DefaultClusterManager');
+module.exports.Host = require('./lib/host/Host');
